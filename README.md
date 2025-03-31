0b378e0a8be15734d9cf38151d540ad26ce8099f
Asphocarp <junyu.dev@gmail.com>
2025-04-01 07:50:45 +0800

    add sync some from 1.19.2

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# ApexMC
+<p align="center">
+    <img src="./src/main/resources/assets/apex_mc/icon.png" alt="ApexMC" width="200"/> 
+</p>
 
-![icon](./src/main/resources/assets/apex_mc/icon.png)
+<h1 align="center">PingSystem</h1>
 
-Make MC Apex Again.
+Ping System for Minecraft. Make MC Apex Again!
 
-## 1. Ping System
+## Ping System
 
-Default hotkey: `z`
+Default hotkey: `c`
 
 Send signal to your team for communication.
 
@@ -22,15 +24,30 @@ Pings at night:
 
 ![pingAtNight](images/pingAtNight.png)
 
-## Dependencies
 
-malilib: [https://www.curseforge.com/minecraft/mc-mods/malilib](https://www.curseforge.com/minecraft/mc-mods/malilib)
+## Build
+
+- use jdk-17 (due to Minecraft 1.19.2 Fabric)
+  - e.g., `"java.import.gradle.java.home": "/opt/homebrew/opt/openjdk@17"` in your vscode user settings Json
+```bash
+export JAVA_HOME=/opt/homebrew/opt/openjdk@17 
+# build
+./gradlew build
+# to show the .jar mod file
+ls -la build/libs
+# run
+./gradlew runClient
+```
+
 
 ## Todo
 
-- [ ] config for mod
-- [ ] support 1.19
-- [ ] ping entity
-- [ ] fix NaN
-- [ ] automatic pathfinding
-- [ ] show on minimap
+- [ ] get current key for info
+- [ ] confirm ping of teammate; team system
+- [ ] fix NaN when looking up or down
+- [ ] (hard) ping entity
+- [ ] (hard) automatic pathfinding
+- [ ] (hard-compatibility) show on minimap
+- [ ] (hard) support forge
+- [x] config for mod with mod menu
+- [x] support 1.19.2
--- src/main/java/com/asilvorcarp/PingPoint.java
+++ src/main/java/com/asilvorcarp/PingPoint.java
@@ -1,7 +1,5 @@
 package com.asilvorcarp;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.math.Vec3d;
@@ -11,37 +9,31 @@ import java.io.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static com.asilvorcarp.ApexMC.LOGGER;
-
 public class PingPoint implements Serializable {
-    // TODO config for this
-    // zero means never
-    public static long SecondsToVanish = 0;
     public UUID id;
     public Vec3d pos;
     public String owner;
+    // the highlight color
     public Color color;
+    // the sound index
+    public byte sound;
     public LocalDateTime createTime;
 
-    public PingPoint(Vec3d pos, String owner) {
-        this.id = UUID.randomUUID();
-        this.pos = pos;
-        this.owner = owner;
-        // default color
-        this.color = new Color(247 / 256f, 175 / 256f, 53 / 256f);
-        this.createTime = LocalDateTime.now();
-    }
-
-    public PingPoint(Vec3d pos, String owner, Color color) {
+    public PingPoint(Vec3d pos, String owner, Color color, byte soundIdx) {
         this.id = UUID.randomUUID();
         this.pos = pos;
         this.owner = owner;
         this.color = color;
+        this.sound = soundIdx;
         this.createTime = LocalDateTime.now();
     }
 
-    public boolean shouldVanish() {
-        if(SecondsToVanish == 0){
+    public PingPoint(Vec3d pos, String owner, int color, byte soundIdx) {
+        this(pos, owner, new Color(color), soundIdx);
+    }
+
+    public boolean shouldVanish(long SecondsToVanish) {
+        if (SecondsToVanish == 0) {
             return false;
         }
         return LocalDateTime.now().minusSeconds(SecondsToVanish).isAfter(createTime);
@@ -57,6 +49,7 @@ public class PingPoint implements Serializable {
         stream.writeDouble(pos.z);
         stream.writeObject(owner);
         stream.writeObject(color);
+        stream.writeByte(sound);
         stream.writeObject(createTime);
     }
 
@@ -71,6 +64,7 @@ public class PingPoint implements Serializable {
         pos = new Vec3d(x, y, z);
         owner = (String) stream.readObject();
         color = (Color) stream.readObject();
+        sound = stream.readByte();
         createTime = (LocalDateTime) stream.readObject();
     }
 
