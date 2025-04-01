8ecfe0bc8edafe475b83fd35fac0c3052dd7d200
Asphocarp <junyu.dev@gmail.com>
2025-04-01 08:23:34 +0800

    add

---
+++ src/client/java/com/asilvorcarp/utils/IRenderer.java
@@ -0,0 +1,33 @@
+package com.asilvorcarp.utils;
+
+import java.util.function.Supplier;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import org.joml.Matrix4f;
+
+public interface IRenderer
+{
+    /**
+     * Called after the vanilla overlays have been rendered
+     */
+    default void onRenderGameOverlayPost(MatrixStack matrixStack) {}
+
+    /**
+     * Called after vanilla world rendering
+     */
+    default void onRenderWorldLast(MatrixStack matrixStack, Matrix4f projMatrix) {}
+
+    /**
+     * Called after the tooltip text of an item has been rendered
+     */
+    default void onRenderTooltipLast(ItemStack stack, int x, int y) {}
+
+    /**
+     * Returns a supplier for the profiler section name that should be used for this renderer
+     */
+    default Supplier<String> getProfilerSectionSupplier()
+    {
+        return () -> this.getClass().getName();
+    }
+}
+
